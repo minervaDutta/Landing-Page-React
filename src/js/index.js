06dece2726645dc495e840e8d5cc03035ed8d580
Minerva Dutta <dutta.minerva@gmail.com>
2022-11-03 17:54:14 -0700

    2nd working component

--- src/js/index.js
+++ src/js/index.js
@@ -8,8 +8,11 @@ import "../styles/index.css";
 //import your own components
 import Home from "./component/home.jsx";
 import Jumbotron from "./component/jumbotron.jsx";
+import Cards from "./component/cards.jsx";
 //import i uppercase (var name)
 
+//var prepend = ("\<div class=\"album py-5 bg-light\"><div class=\"container\"><div class=\"row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3\">");
+
 //render your react application
-ReactDOM.render(<Jumbotron />, document.querySelector("#app"));
+ReactDOM.render(<Cards/>, document.querySelector("#app"));
 //ReactDOM.render( <Home />, document.querySelector("#app"));
